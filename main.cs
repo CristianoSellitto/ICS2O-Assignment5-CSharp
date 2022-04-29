d8f8b7746f8d4f654dcbfff724e5bea29b747c29
CristianoSellit <99354780+CristianoSellitto@users.noreply.github.com>
2022-04-29 16:51:36 +0000

    Added program

--- main.cs
+++ main.cs
@@ -8,7 +8,27 @@ class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello World");
+        string currentAction = "minus";
+        int numberOfIterations;
+        float number = 1;
+        float pi;
+
+        Console.Write("Enter number of iterations: ");
+        numberOfIterations = Convert.ToInt32(Console.ReadLine());
+        pi = (4 / number);
+        while (numberOfIterations > 1) {
+          if (currentAction == "minus") {
+            number = number + 2;
+            pi = pi - (4 / number);
+            currentAction = "plus";
+          } else if (currentAction == "plus") {
+            number = number + 2;
+            pi = pi + (4 / number);
+            currentAction = "minus";
+          }
+          numberOfIterations = numberOfIterations - 1;
+        }
+        Console.WriteLine("π is equal to " + (pi).ToString("0.00"));
         Console.WriteLine("\nFinished.");
     }
 }
